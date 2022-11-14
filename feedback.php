277d98c5a952b766efca3f8b9f8afb553b3f27de
maraty-projects <maupamaupa497@gmail.com>
2022-11-14 17:22:06 +0100

    added validation

--- feedback.php
+++ feedback.php
@@ -1,23 +1,21 @@
 <?php include 'inc/header.php';
 
-$feedback =
-    [
-            'id' => '1',
-            'name' => 'Betty White',
-            'email' => 'beth@hotmail.com',
-            'body' => 'blablabla'
-    ]
-
+$sql = 'SELECT * FROM feedback';
+$result = mysqli_query($conn, $sql);
+$feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
-<h2> Past Feedback</h2>
+<h2> Past Feesdback</h2>
 
 <?php if(empty($feedback)): ?>
     <p class="lead mt3">There is no feedback</p>
 <?php endif; ?>
     <?php foreach($feedback as $item): ?>
-    <div class="card my-3">
-     <div class="card-body">
+    <div class="card my-3 w-75">
+     <div class="card-body text-center">
         <?php echo $item['body']; ?>
+         <div class=""text-secondary mt-2>
+             By <?php echo $item['name']; ?> on <?php echo $item['date']; ?>
+         </div>
      </div>
    </div>
   <?php endforeach; ?>
--- inc/header.php
+++ inc/header.php
@@ -1,3 +1,4 @@
+<?php include 'config/database.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,15 +18,15 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/php-crash/feedback/index.php">Home</a>
+                    <a class="nav-link" href="/php-project/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/php-crash/feedback/feedback.php"
+                    <a class="nav-link" href="/php-project/feedback.php"
                     >Feedback</a
                     >
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/php-crash/feedback/about.php"
+                    <a class="nav-link" href="/php-project/feedback/about.php"
                     >About</a
                     >
                 </li>

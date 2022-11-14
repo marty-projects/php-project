130b3d1175bbffa0858f00d909f0897044cdda73
maraty-projects <maupamaupa497@gmail.com>
2022-11-14 17:00:02 +0100

    added fetching data from mysql db

--- feedback.php
+++ feedback.php
@@ -1,22 +1,26 @@
-<?php include 'inc/header.php'; ?>
-<h2>Feedback</h2>
+<?php include 'inc/header.php';
 
+$feedback =
+    [
+            'id' => '1',
+            'name' => 'Betty White',
+            'email' => 'beth@hotmail.com',
+            'body' => 'blablabla'
+    ]
+
+?>
+<h2> Past Feedback</h2>
+
+<?php if(empty($feedback)): ?>
+    <p class="lead mt3">There is no feedback</p>
+<?php endif; ?>
+    <?php foreach($feedback as $item): ?>
     <div class="card my-3">
      <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+        <?php echo $item['body']; ?>
      </div>
    </div>
+  <?php endforeach; ?>
 
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
-
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
 <?php include 'inc/footer.php'; ?>
 
